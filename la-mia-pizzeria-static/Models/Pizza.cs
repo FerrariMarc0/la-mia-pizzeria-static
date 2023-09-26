7614d00842e1a7739fb0d2ab24a6adc20dbe511b
FerrariMarc0 <marco1010@live.it>
2023-09-26 17:58:28 +0200

    jumbotron

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image_url {  get; set; }
+        public decimal Price { get; set; }
+    }
+}
